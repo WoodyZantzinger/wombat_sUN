9a05006cd9b8c57542a4c2cb454afaa3c2b7bbbb
Woody Zantzinger <woodyzantzinger@WTA-MBP86.local>
2017-03-30 10:38:12 -0400

    added sample bot

---
+++ sUN_bot.py
@@ -0,0 +1,15 @@
+__author__ = 'woodyzantzinger'
+
+def wombat(state, time_left):
+    # Note that the function name MUST be wombat
+    return {
+        'command': {
+            'action': 'turn',
+            'metadata': {
+                'direction': 'right'
+            }
+        },
+        'state': {
+            'hello': 'world'
+        }
+    }
